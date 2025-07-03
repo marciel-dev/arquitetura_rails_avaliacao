f2354596f499b5e779c6058e6068269485c31183
marciel miranda <marciel.miranda>
2025-07-03 11:38:57 +0000

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,23 +8,23 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-# User.create!(
-#   first_name: "Admin Master",
-#   last_name: "MbaOnRails",
-#   email: "admin@mbaonrails.com",
-#   password: "123456",
-#   role: :admin
-# )
+User.create!(
+  first_name: "Admin Master",
+  last_name: "MbaOnRails",
+  email: "admin@mbaonrails.com",
+  password: "123456",
+  role: :admin
+)
 
-# 10.times do |i|
-#   User.create!(
-#     first_name: "Usuário #{i + 1}",
-#     last_name: "MbaOnRails",
-#     email: "user#{i + 1}@mbaonrails.com",
-#     password: "123456",
-#     role: :client
-#   )
-# end
+10.times do |i|
+  User.create!(
+    first_name: "Usuário #{i + 1}",
+    last_name: "MbaOnRails",
+    email: "user#{i + 1}@mbaonrails.com",
+    password: "123456",
+    role: :client
+  )
+end
 
 100.times do
   Product.create!(
